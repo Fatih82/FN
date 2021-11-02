4c8ecc0c8ba6d0313d50c50343e30c8f9868d55f
fatih82 <Fatih82@schule.bremen.de>
2021-11-02 15:32:43 +0100

    ErstesProgramm

---
+++ Arbeit/src/DreiEck.java
@@ -0,0 +1,11 @@
+public class DreiEck {
+    public static void main(String[] args) {
+        double grundseite= 5.7;
+        double hoehe = 5;
+        double flaeninhalt;
+
+        flaeninhalt = hoehe * grundseite / 2;
+
+        System.out.println(flaeninhalt);
+    }
+}
