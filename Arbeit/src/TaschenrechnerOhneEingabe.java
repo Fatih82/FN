dd35177936d011c4aebb777d04f2df479eec1dc0
fatih82 <Fatih82@schule.bremen.de>
2021-11-23 15:20:43 +0100

    ErstesProgramm

---
+++ Arbeit/src/TaschenrechnerOhneEingabe.java
@@ -0,0 +1,17 @@
+public class TaschenrechnerOhneEingabe {
+    public static void main(String[] args) {
+        double a = 7;
+        double b = 30;
+        double summe = (a+b);
+        double differenz = (a-b);
+        double produkt = (a*b);
+        double Quotient = (a/b);
+        System.out.println("a= "+a);
+        System.out.println("b= "+b);
+        System.out.println("summe = "+summe);
+        System.out.println("differenz = "+differenz);
+        System.out.println("produkt = "+produkt);
+        System.out.println("Quotient= "+Quotient);
+
+    }
+}
