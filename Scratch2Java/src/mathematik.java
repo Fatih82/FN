1a1a0e4bf3c73a1d8045948ffd318b296e6a3c49
fatih82 <Fatih82@schule.bremen.de>
2021-11-30 14:14:39 +0100

    ErstesProgramm

--- Scratch2Java/src/mathematik.java
+++ Scratch2Java/src/mathematik.java
@@ -2,9 +2,12 @@ public class mathematik {
     public static void main(String[] args) {
         int radius=10;
         double flaeche= Math.pow(radius,2)*Math.PI;
+        // Math.pow wird meistens verwendet um die Potzen von Zahlen zu berechenen also berechnet x hoch y
+        // Math.PI Ermittlung der Kreiszahl PI gleich 3,14
         double umsaztSteuer= 34506.74;
         System.out.println(flaeche);
         System.out.println(Math.floor(umsaztSteuer));
+        //
         System.out.println(Math.floor(umsaztSteuer*10)/10);
         System.out.println(Math.min(300,flaeche));
         System.out.println(Math.max(300,flaeche));
